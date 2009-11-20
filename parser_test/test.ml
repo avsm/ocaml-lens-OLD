79af9a74e83833f9f13695902a84fcc9c90cb610
Anil Madhavapeddy <anil@recoil.org>
2009-11-19 17:40:49 -0800

    switch tests over to use the Lwt JSON parser

--- parser_test/test.ml
+++ parser_test/test.ml
@@ -7,14 +7,15 @@ open Value
 open Parser_json
 
 let parse_file fname =
-  let buf = Buffer.create 1024 in
-  Lwt_stream.iter (fun l -> Buffer.add_string buf (l ^ "\n")) (lines_of_file fname) >>
-  let s = Buffer.contents buf in
-  let v = Parser_json.of_string s in
-  printf "%s: %s\n%!" fname (Value.to_string v);
-  return ()
-
+  Lens_unix.with_file fname
+    (fun env ->
+      let s = Lens.char_stream env in
+      lwt v = Parser_json.of_stream s in
+      printf "%s: %s\n%!" fname (Value.to_string v);
+      return ()
+    )
 let _ =
-  let f = [ "basic.json" ] in
+  let f = [ "basic.json"; "list.json"; "broken1.json" ] in
+  
   Lwt_main.run (
     Lwt_util.iter parse_file f)
